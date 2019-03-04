7a76814b770295fa3187b7ce026342ff23e2d115
AlejandroCN7 <alejandroac79@gmail.com>
2019-03-04 17:36:09 +0100

    Realizados los primeros ejercicios correspondientes a tarea2.html de la asignatura

---
+++ ejercicios/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('hola_mundo', views.hola_mundo),	# entrada str
+	path('ejercicio1/<lista>', views.ejercicio1), # entrada str, dada una lista de strings te dice el número de palabras con tamaño mayor a 2 y que empizan y acaban por la misma letra
+	path('ejercicio2/<lista>', views.ejercicio2), # entrada str, dada una lista de números, te elimina aquellos valores que están repetidos
+	path('ejercicio3/<entrada>', views.ejercicio3), # entrada str, dado un string, te devuelve una palabra con las 2 primeras y ultimas letras del string o cadena vacío si tiene un número de letras inferior a 2
+	path('ejercicio4/<entrada>', views.ejercicio4), # entrada str, añade ing a la entrada, si ya acaba en ing añade ly, si el tamaño es menor que 3 la deja como está
+	path('ejercicio5/<entrada>', views.ejercicio5), # entrada str, ejercicio 5
+	]
---
+++ ejercicios/views.py
@@ -0,0 +1,119 @@
+from django.shortcuts import HttpResponse
+import random
+import re, string
+import os
+
+# Create your views here.
+
+def hola_mundo(request):
+
+	salida = '''<html>
+  			Hola mundo
+		    </html>''' 
+
+	return HttpResponse(salida)
+
+def ejercicio1(request, lista):
+
+	lista=lista.split(",")
+
+	contador=0
+	for cadena in lista:
+		if(len(cadena)>1 and cadena[0]==cadena[len(cadena)-1]):
+			contador +=1
+
+	salida = '''<html>
+  			Número de palabras aceptadas %s
+		    </html>'''%(str(contador)) 
+
+	return HttpResponse(salida)
+
+def ejercicio2(request, lista):
+
+	lista=lista.split(",")
+
+	lista = set(lista)
+	salida = '''<html>
+  			Números eliminando repetidos: %s
+		    </html>'''%(str(lista)) 
+	
+	return HttpResponse(salida)
+
+def ejercicio3(request, entrada):
+
+	resultado=""
+	if(len(entrada)<2):
+		pass
+	else:
+		resultado += entrada[0:2] + entrada[len(entrada)-2:len(entrada)]
+
+	salida = '''<html>
+  			Dos primeras y dos últimas letras: %s
+		    </html>'''%(resultado) 
+
+	return HttpResponse(salida)
+
+def ejercicio4(request, entrada):
+	
+	if(len(entrada)<3):
+		pass
+	elif(entrada[len(entrada)-3:len(entrada)]=="ing"):
+		entrada += "ly"
+	else:
+		entrada += "ing"
+
+	salida = '''<html>
+  			Palabra transformada: %s
+		    </html>'''%(entrada)
+
+	return HttpResponse(salida)
+
+def ejercicio5(request, entrada):
+
+	module_dir = os.path.dirname(__file__)  # get current directory
+	file_path = os.path.join(module_dir, entrada)
+	f = open(file_path,"r")
+	mensaje = f.read()
+	f.close()
+
+	# Eliminamos los signos de puntuación
+	mensaje = re.sub('[%s]' % re.escape(string.punctuation), '', mensaje)
+	# Tambien debemos de eliminar los saltos de línea que puedan aparecer en el documento
+	mensaje = re.sub('[%s]' % re.escape("\n"), '', mensaje)
+	# Separamos cada palabra de tal forma que trabajamos con una lista
+	mensaje = mensaje.split(" ")
+
+	# Creamos el diccionario llamado "mimic" del ejercicio
+	mimic = {}
+	for i in range(0, len(mensaje)):
+		palabra = mensaje[i]
+
+		# Si es la última palabra y nunca antes ha aparecido debemos de crearle una lista vacía como definición
+		if (i == len(mensaje) - 1):
+			if (palabra not in mimic):
+				mimic[palabra] = list()
+		else:
+			siguiente_palabra = mensaje[i + 1]
+			# En caso de que la palabra ya estuviera en el diccionario añadimos la palabra que lo sigue en su lista de definición
+			if palabra in mimic:
+				mimic[palabra].append(siguiente_palabra)
+			# Si es la primera vez que aparece debemos de crear la lista con la palabra siguiente de la que estamos analizando
+			else:
+				mimic[palabra] = [siguiente_palabra]
+
+	# Ya tenemos el diccionario mimic creado
+	palabra=mensaje[0]
+	texto=""
+	texto+=palabra
+	for i in range(0,30):
+		palabra = random.choice(mimic[palabra])
+		texto += " "
+		texto += palabra
+		if (palabra not in mimic or mimic[palabra]==[]):
+			palabra = mensaje[0]
+
+	salida = '''<html>
+  			%s
+		    </html>'''%(texto)
+
+	return HttpResponse(salida)
