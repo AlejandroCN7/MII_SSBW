ea122bff8079247612441251c1b3d6a6bf80179a
AlejandroCN7 <alejandroac79@gmail.com>
2019-03-30 20:25:38 +0100

    Realizado visualizador de los datos a partir de tablas búscadas por actor en la app pelis

--- pelis/urls.py
+++ pelis/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
 	path('hola_mundo', views.hola_mundo),	# entrada str
+	path('pelis que sale/<actor>', views.pelisQueSale, name ="PelisQueSale"), # Parte pymongo de tarea 5
+	path('procesandoActor', views.procesandoActor),
+	path('formulario', views.formulario, name= "formulario"),
+	path('visualizador/<id>', views.mostrarPelicula, name= "visualizador"),
 	]
--- pelis/views.py
+++ pelis/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import HttpResponse, render, HttpResponseRedirect
 from mongoengine import *
+from .models import Pelis
+from django.urls import reverse
+import re
 
 import os
 
@@ -17,3 +20,44 @@ def hola_mundo(request):
 
 #-----------------------------------------------------------------------------------------------------------------------
 
+#-----------------------------------------------------------------------------------------------------------------------
+# Consultas con mongoengine
+def pelisQueSale(request,actor):
+
+	regex = re.compile(actor)
+	peliculas = Pelis.objects(actors=regex)
+	context = {
+		'lista': peliculas,
+		'actor': True,
+		'resultados': peliculas.count(),
+	}
+
+	return render(request,"tabla.html",context)
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+#Formulario y su posterior procesamiento desde backend
+def formulario(request):
+
+	return render(request,"form.html")
+
+def procesandoActor(request):
+
+	actor = request.POST.get('actor')
+
+	return HttpResponseRedirect(reverse('PelisQueSale',args=[actor]))
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Para sacar la vista de la película seleccionada desde la tabla
+def mostrarPelicula(request,id):
+
+	pelicula = Pelis.objects(id=id) # Como lo buscamos por id, nos va a salir una lista de un solo elemento
+	context ={
+		'pelicula': pelicula[0]
+	}
+
+	return render(request,"visualizador.html",context)
+#-----------------------------------------------------------------------------------------------------------------------
